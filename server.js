4d934ef24ad51ca73defc0722cceb0dbb534602c
Christine Lee <59518411+christineslee@users.noreply.github.com>
2022-09-29 21:54:29 -0400

    Second Commit

--- server.js
+++ server.js
@@ -12,7 +12,19 @@ app.listen(PORT, () => {
 })
 
 //====greetings
-app.get('/greeting/:name', (req, res) => {
-    const {name} = req.params
-    res.send(`Hello, ${name}`)
+// app.get('/greeting/:name', (req, res) => {
+//     const {name} = req.params
+//     res.send(`Hello, ${name}`)
+// })
+
+//====tip calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const {total, tipPercentage} = req.params
+
+    //Both Works -> Number or parseInt
+    res.send('Tip: $'+ (Number(`${total}`)/Number(100))*Number(`${tipPercentage}`))
+    // res.send('Tip: $'+ (parseInt(`${total}`)/parseInt(100))*parseInt(`${tipPercentage}`))
+    //Need string to use res.send -> added 'Tip: $'
+
 })
+
