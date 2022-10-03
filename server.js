41855a4b981dde2b207880d4c0720358e638d71d
Christine Lee <59518411+christineslee@users.noreply.github.com>
2022-10-03 16:14:44 -0400

    Fourth Commit

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.listen(PORT, () => {
 })
 
 //====greetings
-// app.get('/greeting/:name', (req, res) => {
-//     const {name} = req.params
-//     res.send(`Hello, ${name}`)
-// })
+app.get('/greeting/:name', (req, res) => {
+    const {name} = req.params
+    res.send(`Hello, ${name}`)
+})
 
 //====tip calculator
 app.get('/tip/:total/:tipPercentage', (req, res) => {
@@ -29,6 +29,11 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 })
 
 //====Magic 8 ball
+const answer = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+const randRes = answer[Math.floor(Math.random() * answer.length)] 
+
 app.get('/magic/:question', (req,res) => {
-    const question = 
+
+    const {question} = req.params 
+    res.send(`Question: ${question}? Answer: ${randRes}`)
 })
